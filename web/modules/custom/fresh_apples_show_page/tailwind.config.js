1697d8c39bd4bfbf86d6f76e1f80fc21314e6974
Adam Youssef <adam.youssef@weareplanet.com>
2024-12-18 22:45:37 +0100

    Add more fields to show content type

---
+++ web/modules/custom/fresh_apples_show_page/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    './templates/**/*.html.twig',
+  ],
+  theme: {
+    dropShadow: {
+      'custom': '0 0 1px black', // Add your custom drop-shadow here
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+};
---
+++ web/modules/fresh_apples_show_page/src/Service/FreshApplesShowPageService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\fresh_apples_show_page\Service;
+
+use Drupal\node\NodeInterface;
+
+class FreshApplesShowPageService {
+
+  public function prepareShowData(NodeInterface $node) {
+    $test = 100;
+
+  }
+
+}
