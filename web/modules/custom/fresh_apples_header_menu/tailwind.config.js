570ed41eea3af960d443c0919b513b2270815b91
Adam Youssef <adam.youssef@weareplanet.com>
2024-12-21 00:51:40 +0100

    Add header

---
+++ web/modules/custom/fresh_apples_header_menu/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    './templates/block--header-menu.html.twig',
+  ],
+  theme: {
+    dropShadow: {
+      'custom': '0 0 1px black', // Add your custom drop-shadow here
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+};
